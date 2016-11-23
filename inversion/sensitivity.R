ac7b6f89dbbb5e5464b988cf8a7538a6c4f0a39a
Shawn P. Serbin <sserbin@bnl.gov>
2016-11-23 11:45:55 -0500

    A couple more fixes and updates

--- inversion/sensitivity.R
+++ inversion/sensitivity.R
@@ -1,10 +1,10 @@
 source("common.R")
 
-outdir <- "sensitivity"
-dir.create(outdir, showWarnings = FALSE)
+outdir <- paste("sensitivity", format(Sys.time(), format="%Y%m%d_%H%M%S"), sep = "_")
+if (! file.exists(outdir)) dir.create(outdir,recursive=TRUE)
 link_ed(outdir)
 
-pft <- "late_hardwood"
+pft <- "temperate.Late_Hardwood"
 pft_pecan <- "temperate.Late_Hardwood"
 dbh <- 40
 lai <- getvar("LAI_CO", dbh, pft)
@@ -26,7 +26,7 @@ edr_sensitivity <- function(param_name, minval, maxval, steps = 7) {
                        trait.values = trait.values,
                        output.path = outdir,
                        dbh = 40,
-                       pft = "late_hardwood")
+                       pft = paste0(pft))
         out[,i] <- alb
     }
     return(out)
@@ -34,33 +34,37 @@ edr_sensitivity <- function(param_name, minval, maxval, steps = 7) {
 
 
 clumping_factor <- edr_sensitivity("clumping_factor", 0, 1)
-png("clumping.png")
+png(paste0(outdir,"/","clumping.png"))
 matplot(clumping_factor, type='l', lty = 1, main = "Clumping factor")
 dev.off()
 
 orient_factor <- edr_sensitivity("orient_factor", -0.5, 0.5)
-png("orient.png")
+png(paste0(outdir,"/","orient.png"))
 matplot(orient_factor, type='l', lty = 1, main = "orient factor")
 dev.off()
 
 sla <- edr_sensitivity("SLA", 10, 100)
-png("sla.png")
+png(paste0(outdir,"/","sla.png"))
 matplot(sla, type='l', lty = 1, main = "SLA")
 dev.off()
 
 height_allom_1 <- edr_sensitivity("b1Ht", -1, 1)
+png(paste0(outdir,"/","b1Ht.png"))
 matplot(height_allom_1, type='l', lty = 1, main = "height_allom_1")
+dev.off()
 
 height_allom_2 <- edr_sensitivity("b2Ht", -1, 1)
+png(paste0(outdir,"/","b2Ht.png"))
 matplot(height_allom_2, type='l', lty = 1, main = "height_allom_2")
+dev.off()
 
 leaf_allom_1 <- edr_sensitivity("b1Bl_large", 0.1, 0.9)
-png("b1Bl_large.png")
+png(paste0(outdir,"/","b1Bl_large.png"))
 matplot(leaf_allom_1, type = 'l', lty = 1, main = "leaf_allom_1")
 dev.off()
 
 leaf_allom_2 <- edr_sensitivity("b2Bl_large", 0.7, 1.5)
-#png("b2Bl_large.png")
+png(paste0(outdir,"/","b2Bl_large.png"))
 matplot(leaf_allom_2, type = 'l', lty = 1, main = "leaf_allom_2")
-#dev.off()
+dev.off()
 
