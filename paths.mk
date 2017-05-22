8e3e096670da52a767d0a00db8f75cb30e8fe50c
Alexey Shiklomanov <ashiklom@bu.edu>
2017-05-22 10:24:43 -0400

    Deprecated .mk files

--- paths.mk
+++
@@ -1,6 +0,0 @@
-# Example
-# ED_EXE := /data/sserbin/Modeling/ED2.git.modularize.EDR/ED/build/ed_2.1-opt
-# EDR_EXE := /data/sserbin/Modeling/ED2.git.modularize.EDR/EDR/build/ed_2.1-opt
-#
-ED_EXE := /data/sserbin/Modeling/ED2.git.latest/ED/build/ed_2.1-opt
-EDR_EXE := /data/sserbin/Modeling/ED2.git.latest/EDR/build/ed_2.1-opt
