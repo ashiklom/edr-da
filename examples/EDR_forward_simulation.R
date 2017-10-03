9268fa99534723f318fa12b8e1a6e77c617d7e33
Shawn P. Serbin <sserbin@bnl.gov>
2017-10-03 14:28:43 -0400

    Updates to forward simulation

--- examples/EDR_forward_simulation.R
+++ examples/EDR_forward_simulation.R
@@ -7,7 +7,7 @@
 #
 #
 #
-#    --- Last updated:  08.29.2017 By Shawn P. Serbin <sserbin@bnl.gov>
+#    --- Last updated:  10.03.2017 By Shawn P. Serbin <sserbin@bnl.gov>
 ####################################################################################################
 
 
@@ -25,11 +25,6 @@ PEcAn.logger::logger.setLevel("INFO")
 ## Load libraries
 library(redr)
 library(magrittr)
-
-## Setup options
-plot_albedo <- TRUE #TRUE/FALSE
-generate_summary_figs <- TRUE #TRUE/FALSE
-hidden <- FALSE  #TRUE/FALSE  # output to a hidden (e.g. .forward_sim_output) folder?
 #--------------------------------------------------------------------------------------------------#
 
 
@@ -45,13 +40,21 @@ PEcAn.logger::logger.info(paste0("Running simulation in dir: ",prefix))
 
 #--------------------------------------------------------------------------------------------------#
 ## Run ED2
-fft_plot <- "NC22"
-css_in <- '/data/sserbin/Modeling/edr-da/ed-inputs/sites/US-WCr/NASA_FFT/US-WCr.Inv2010.NC22.lat45.5lon-90.5.css'
-pss_in <- '/data/sserbin/Modeling/edr-da/ed-inputs/sites/US-WCr/NASA_FFT/US-WCr.Inv2010.NC22.lat45.5lon-90.5.pss'
-site_in <- '/data/sserbin/Modeling/edr-da/ed-inputs/sites/US-WCr/NASA_FFT/US-WCr.Inv2010.NC22.lat45.5lon-90.5.site'
+fft_plot <- "BH02"  # NC22, BH02
+#css_in <- '/data/sserbin/Modeling/edr-da/ed-inputs/sites/US-WCr/NASA_FFT/US-WCr.Inv2010.NC22.lat45.5lon-90.5.css'
+#pss_in <- '/data/sserbin/Modeling/edr-da/ed-inputs/sites/US-WCr/NASA_FFT/US-WCr.Inv2010.NC22.lat45.5lon-90.5.pss'
+#site_in <- '/data/sserbin/Modeling/edr-da/ed-inputs/sites/US-WCr/NASA_FFT/US-WCr.Inv2010.NC22.lat45.5lon-90.5.site'
+css_in <- '/data/sserbin/Modeling/edr-da/ed-inputs/sites/BH02_site_1-25665/FFT.2008.lat43.3724lon-89.9071.css'
+pss_in <- '/data/sserbin/Modeling/edr-da/ed-inputs/sites/BH02_site_1-25665/FFT.2008.lat43.3724lon-89.9071.pss'
+site_in <- '/data/sserbin/Modeling/edr-da/ed-inputs/sites/BH02_site_1-25665/FFT.2008.lat43.3724lon-89.9071.site'
+
+site_lat <- 43.3724 #45.5 #43.3724
+site_lon <- -89.9071 #-90.5 #-89.9071
+
 ed2in_changes <- list(IMONTHA = 07, IDATEA = 15, IYEARA = 2006,
                       IMONTHZ = 08, IDATEZ = 31, IYEARZ = 2006)
 datetime <- ISOdate(2006, 08, 26, 16, 00, 00)
+
 genrun <- generate_run(prefix = prefix,
                        site_lat = site_lat,
                        site_lon = site_lon,
@@ -75,7 +78,8 @@ edr_setup <- setup_edr(prefix, edr_exe_path = edr_exe_path)
 
 #--------------------------------------------------------------------------------------------------#
 ## Setup priors
-load('priors/sunlit_meanjp.RData')
+#load('priors/sunlit_meanjp.RData') # old prior
+load('priors/mvtraits_priors.RData')
 
 # Params vector
 # Temperate.Early_Hardwood: 1:8
@@ -85,9 +89,9 @@ load('priors/sunlit_meanjp.RData')
 #   8 -- orient_factor
 # Temperate.North_Mid_Hardwood: 9:16
 # Temperate.Late_Hardwood: 17:24
-pft_end <- c(temperate.Early_Hardwood = 8,
-             temperate.North_Mid_Hardwood = 16,
-             temperate.Late_Hardwood = 24)
+pft_end <- c(temperate.North_Mid_Hardwood = 8,
+             temperate.Late_Hardwood = 16,
+             temperate.Late_Conifer = 24)
 
 param_sub <- function(i, params) {
   param_seq <- (pft_end[i] - 7):pft_end[i]
@@ -100,15 +104,16 @@ inits_function <- function() {
   for (i in seq_along(pft_end)) {
     pft <- names(pft_end)[i]
     # PROSPECT prior
-    samples <- c(samples, mvtnorm::rmvnorm(1, means[pft,], covars[pft,,]))
+    #samples <- c(samples, mvtnorm::rmvnorm(1, means[pft,], covars[pft,,]))
+    samples <- c(samples, mvtnorm::rmvnorm(1, means[pft,], covars[,,pft]))
     # ED priors
     #samples <- c(samples, runif(1, 0, 1), runif(1, -0.5, 0.5)) # clumping and orient factor
-    samples <- c(samples, runif(1, 0.8, 0.87), runif(1, 0.15, 0.2)) # clumping and orient factor
+    samples <- c(samples, runif(1, 0.8, 0.87), runif(1, 0.01, 0.15)) # clumping and orient factor
+    #samples <- c(samples, runif(1, 0.55, 0.98), runif(1, -0.5, 0.5)) # clumping and orient factor
   }
-  
-  name_vec <- rep(c("N","Cab","Car","Cw","Cm","leaf_mass_per_area","clumping_factor","orient_factor"),length(pft_end))
-  samples[which(name_vec=="leaf_mass_per_area")] <- 1/samples[which(name_vec=="leaf_mass_per_area")]*1000 # convert to SLA
-  names(samples) <- rep(c('N', 'Cab', 'Car', 'Cw', 'Cm', 'SLA', 'clumping_factor', 'orient_factor'),3)
+  name_vec <- rep(c("N","Cab","Car","Cw","Cm","SLA","clumping_factor","orient_factor"),length(pft_end))
+  names(samples) <- name_vec
+  samples <- c(samples, residual = rlnorm(1, log(0.001), 2.5))
   return(samples)
 }
 
@@ -168,42 +173,54 @@ model <- function(params) {
 #--------------------------------------------------------------------------------------------------#
 ## Run EDR
 # Test that the model works
-run_model <- model(inits_function())
-head(run_model)
+# single run
+#run_model <- model(inits_function())
+#head(run_model)
 
+# run X times
+x <- 10
+edr_albedo <- array(data=NA, c(x,2101))
+for (i in seq_along(1:dim(edr_albedo)[1])) {
+  print(paste0(" Simulation: ",i))
+  edr_albedo[i,] <- model(inits_function())
+  #tryCatch(edr_albedo[i,] <- model(inits_function()), finally = print("Hello"))
+}
 
-
-#spectra_list <- list(temperate.Early_Hardwood = PEcAnRTM::prospect(c(1.4, 40, 0.01, 0.01), 4, TRUE),
-#                     temperate.North_Mid_Hardwood = PEcAnRTM::prospect(c(1.6, 50, 0.01, 0.012), 4, TRUE),
-#                     temperate.Late_Hardwood = PEcAnRTM::prospect(c(1.8, 60, 0.01, 0.012), 4, TRUE))
-#trait.values <- list(temperate.Early_Hardwood =  list(orient_factor = 0.1, clumping_factor = 0.86, SLA=15),
-#                     temperate.North_Mid_Hardwood = list(orient_factor = 0.1, clumping_factor = 0.86, SLA=7),
-#                     temperate.Late_Hardwood = list(orient_factor = 0.1, clumping_factor = 0.86, SLA=15))
-#edr_args <- list(spectra_list = spectra_list, trait.values = trait.values,par.wl = 400:2499,nir.wl = 2500,
-#                 datetime = datetime)
-#run_model <- run_edr(dir=prefix,edr_args=edr_args)
+edr_albedo_mean <- colMeans(edr_albedo, na.rm=T)
+edr_albedo_quant <- apply(edr_albedo,2,quantile,na.rm=T,probs=c(0,0.05,0.95,1))
 #--------------------------------------------------------------------------------------------------#
 
 
 #--------------------------------------------------------------------------------------------------#
 ## Generate comparison figure
-aviris.data <- read.csv('/data/sserbin/Modeling/edr-da/aviris/NASA_FFT/allPlotsAggregated_NIT_A_SpecJoined.csv',header = T,sep=",")
-aviris.spectra <- as.matrix(droplevels(aviris.data[,paste0("band_",seq(1,224,1))]))
-spectra <- aviris.spectra[which(aviris.data$iPLOT==fft_plot),]
-av.waves <- read.csv('/data/sserbin/Modeling/edr-da/aviris/NASA_FFT/aviris_c_wavelength.csv',header = T,sep=",")
-
-waves <- seq(400,2500,1)
-png(file.path(prefix,'edr','EDR_vs_AVIRIS_Albedo.png'),width=4900, height =2700,res=400)
-par(mfrow=c(1,1), mar=c(4.3,4.5,1.0,1), oma=c(0.1,0.1,0.1,0.1)) # B L T R
-plot(waves,unlist(run_model)*100,type="l",lwd=3,ylim=c(0,60),xlab="Wavelength (nm)",ylab="Reflectance (%)",
-     cex.axis=1.5, cex.lab=1.7,col="black")
-rect(par("usr")[1], par("usr")[3], par("usr")[2], par("usr")[4], col = 
-       "grey80")
-lines(waves,unlist(run_model)*100,lwd=3, col="green4")
-lines(unlist(av.waves[,1]), spectra*0.01,lwd=2)
-legend("topleft",legend=c("Modeled","Measured"), lwd=3,col=c("green4","black"))
-dev.off()
-
+if (generate_summary_figs) {
+  waves <- seq(400,2500,1)
+  aviris.data <- read.csv('/data/sserbin/Modeling/edr-da/aviris/NASA_FFT/allPlotsAggregated_NIT_A_SpecJoined.csv',
+                          header = T,sep=",")
+  aviris.spectra <- as.matrix(droplevels(aviris.data[,paste0("band_",seq(1,224,1))]))
+  spectra <- aviris.spectra[which(aviris.data$iPLOT==fft_plot),]
+  av.waves <- read.csv('/data/sserbin/Modeling/edr-da/aviris/NASA_FFT/aviris_c_wavelength.csv',header = T,sep=",")
+  #spectra_quant <- apply(spectra,2,quantile,na.rm=T,probs=c(0,1))
+  #spectra_mean <- colMeans(spectra)
+  png(file.path(prefix,'edr','EDR_vs_AVIRIS_Albedo.png'),width=4900, height =2700,res=400)
+  par(mfrow=c(1,1), mar=c(4.3,4.5,1.0,1), oma=c(0.1,0.1,0.1,0.1)) # B L T R
+  plot(waves,unlist(edr_albedo_mean)*100,type="l",lwd=3,ylim=c(0,60),xlab="Wavelength (nm)",ylab="Reflectance (%)",
+       cex.axis=1.5, cex.lab=1.7,col="black")
+  rect(par("usr")[1], par("usr")[3], par("usr")[2], par("usr")[4], col = 
+         "grey80")
+  polygon(c(waves ,rev(waves)),c(edr_albedo_quant[3,]*100, rev(edr_albedo_quant[2,]*100)),
+          col="#99CC99",border=NA)
+  lines(waves,unlist(edr_albedo_mean)*100,lwd=3, col="green4")
+  if (is.null(dim(spectra)[1])) {
+    lines(unlist(av.waves[,1]), as.vector(spectra)*0.01,lwd=2)
+  } else {
+    for (i in seq_along(1:dim(spectra)[1])) {
+      lines(unlist(av.waves[,1]), spectra[i,]*0.01,lwd=2)
+    }
+  }
+  legend("topleft",legend=c("Modeled","Measured"), lwd=3,col=c("green4","black"))
+  dev.off()
+}
 #--------------------------------------------------------------------------------------------------#
 
 
