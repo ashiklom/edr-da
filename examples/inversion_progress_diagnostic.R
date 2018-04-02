abdd287c2314246ccaa23b2a69a1ca73670d7311
Alexey Shiklomanov <ashiklom@bu.edu>
2018-04-02 09:48:14 -0400

    Remove lots of old scripts

--- examples/inversion_progress_diagnostic.R
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env Rscript
-library(BayesianTools)
-
-samples <- readRDS('.edr_inversion/inversion_samples_progress.rds')
-
-pdf('progress_samples.pdf')
-plot(samples)
-dev.off()
--- inversion/deprecated/readhdf5.R
+++
@@ -1,5 +0,0 @@
-library(ncdf4)
-
-nc <- nc_open("testdir/history-S-2004-07-01-120000-g01.h5")
-
-ncvar_get(nc, "LAI_CO")
