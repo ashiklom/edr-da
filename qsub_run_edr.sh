2fd363e8e72579c8bc709cb4bbd237da5d059e49
Alexey Shiklomanov (scc) <ashiklom@bu.edu>
2018-04-18 17:31:27 -0400

    Split up ensemble run_edr

--- qsub_run_edr.sh
+++ qsub_run_edr.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-#$ -q "geo*"
-#$ -pe omp 16
 #$ -l h_rt=04:00:00
 #$ -j y
 #$ -o logs_edr
+#$ -t 1-50
 
 /usr3/graduate/ashiklom/.singularity/Rscript scripts/ensemble/02_run_edr.R \
-  --ncores=16 \
-  $@
+    --ens=$SGE_TASK_ID \
+    $@
--- scripts/ensemble/02_run_edr.R
+++ scripts/ensemble/02_run_edr.R
@@ -7,21 +7,19 @@ import::from(here, inhere = here)
 import::from(lubridate, as_date, year, month, mday)
 import::from(progress, progress_bar)
 
-import::from(foreach, foreach, "%dopar%")
-
-import::from(snow, makeCluster)
-import::from(doSNOW, registerDoSNOW)
+import::from(foreach, foreach, "%do%")
 
 # [1] "OF05"  "IDS36" "SF03"  "BH07"  "AK60"  "OF02"  "BH05" 
 
 args <- commandArgs(trailingOnly = TRUE)
 if (interactive()) {
-  args <- c("--site=OF05", "--ncores=2")
+  args <- c("--site=OF05")
 }
 
 argl <- OptionParser() %>%
   add_option("--site", action = "store", type = "character", default = "AK60") %>%
-  add_option("--ncores", action = "store", type = "integer", default = 1) %>%
+  add_option("--ens", action = "store", type = "integer", default = 1) %>%
+  add_option("--by", action = "store", type = "integer", default = 10) %>%
   parse_args(args)
 print(argl)
 
@@ -29,6 +27,13 @@ site <- argl$site
 ensemble_root <- inhere("ensemble_outputs", "msp_hf20180402")
 site_dir <- list.files(ensemble_root, argl$site, full.names = TRUE)
 stopifnot(length(site_dir) == 1)
+
+write_line <- function(a, b, file, append = TRUE, ...) {
+  btxt <- paste(b, collapse = ",")
+  txt <- paste(a, btxt, sep = ",")
+  cat(txt, "\n", file = file, append = append, ...)
+  txt
+}
 
 simulate_spec_ensemble <- function(ens, site_dir, by = 10) {
   ens_dir <- file.path(site_dir, sprintf("ens_%03d", ens))
@@ -40,32 +45,29 @@ simulate_spec_ensemble <- function(ens, site_dir, by = 10) {
     lubridate::as_date()
   has_dates <- has_dates[has_dates > lubridate::as_date("2000-01-01")]
   dates <- seq(min(has_dates), max(has_dates), by = by)
-  message("Running from ", min(dates) " to ", max(dates))
+  message("Running from ", min(dates), " to ", max(dates))
   message("In total, ", length(dates), " runs.")
   ed2in <- PEcAn.ED2::read_ed2in(file.path(ens_dir, "ED2IN"))
   ed2in$RK4_TOLERANCE <- 1e-5
-  run_edr_date2 <- function(date, ...) {
-    message("Running date: ", date)
-    purrr::safely(run_edr_date)(date, ...)
-  }
   PEcAn.logger::logger.setLevel("INFO")
-  purrr::map(
-    dates,
-    run_edr_date2,
-    ed2in = ed2in, trait_values = trait_values
-  )
+  outfile <- file.path(ens_dir, "spectra_sims.csv")
+  file.remove(outfile)
+  file.create(outfile)
+  wavecols <- paste0("wave_", 400:2500)
+  write_line("date", wavecols, outfile)
+  foreach(date = dates, .errorhandling = "pass") %do% {
+    message("Running date: ", date)
+    spec <- run_edr_date(
+      date = date,
+      ed2in = ed2in,
+      trait_values = trait_values
+    )
+    write_line(date, spec, outfile)
+    spec
+  }
 }
 
 ############################################################
 PEcAn.logger::logger.setLevel("INFO")
-ens_vec <- seq(1, 50)
-cl <- makeCluster(argl$ncores, outfile = "")
-registerDoSNOW(cl)
-opts <- list(progress = function(n) message("Running ensemble: ", n))
-result <- foreach(
-  ens = ens_vec,
-  .packages = c("magrittr", "redr"),
-  .options.snow = opts
-) %dopar%
-  purrr::safely(simulate_spec_ensemble)(ens, site_dir, by = 5)
-saveRDS(result, file.path(site_dir, "spec_sims.rds"))
+out <- simulate_spec_ensemble(argl$ens, site_dir, by = argl$by)
+saveRDS(out, file.path(site_dir, sprintf("ensemble_%03d.rds", argl$ens)))
