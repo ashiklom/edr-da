221e1c1e3b626a8ed27c81520c1384dcf6b9a1dd
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2018-12-06 17:17:56 -0500

    Add LAI observations to plots, and other enhancements

--- scripts/drake_master.R
+++ scripts/drake_master.R
@@ -53,10 +53,10 @@ pre_plan <- drake_plan(
     facet_wrap(vars(parameter), scales = "free_y") +
     theme(axis.text.x = element_blank(),
           axis.ticks.x = element_blank()),
-  site_files <- purrr::map_chr(
-    selected_sites,
-    ~head(list.files(file.path("sites", .x), "css$", full.names = TRUE), 1)
-  ) %>%
+  site_files = selected_sites %>%
+    purrr::map_chr(
+      ~head(list.files(file.path("sites", .x), "css$", full.names = TRUE), 1)
+    ) %>%
     setNames(selected_sites),
   site_df = purrr::map_dfr(site_files, PEcAn.ED2::read_css, .id = "site") %>%
     tibble::as_tibble() %>%
@@ -65,30 +65,34 @@ pre_plan <- drake_plan(
       ipft = get_ipft(ipft),
       hite = dbh2h(dbh, ipft)
     ) %>%
-    dplyr::group_by(site, year, ipft, hite) %>%
-    dplyr::summarize(
-      dbh = mean(dbh),
-      nplant = sum(nplant)
-    ) %>%
-    dplyr::arrange(desc(hite)) %>%
-    dplyr::ungroup() %>%
+    ## dplyr::group_by(site, year, ipft, hite) %>%
+    ## dplyr::summarize(
+    ##   dbh = mean(dbh),
+    ##   nplant = mean(nplant)
+    ## ) %>%
+    ## dplyr::ungroup() %>%
     dplyr::group_by(site, year) %>%
+    dplyr::arrange(desc(hite)) %>%
     dplyr::mutate(cohort = dplyr::row_number()) %>%
     dplyr::ungroup(),
+  # TODO: Optimize this somehow. data.table? Matrix math?
   site_lai_samples = site_df %>%
     dplyr::left_join(
       tidy_params %>%
         dplyr::filter(parameter %in% c("b1Bl", "SLA", "clumping_factor")) %>%
         tidyr::spread(parameter, value) %>%
-        dplyr::select(sample, ipft, SLA, b1Bl, clumping_factor)
+        dplyr::select(sample, pft, ipft, SLA, b1Bl, clumping_factor) %>%
+        purrr::modify(unname) %>%
+        dplyr::sample_n(10)
     ) %>%
     dplyr::mutate(
       bleaf = size2bl(dbh, b1Bl, purrr::map_dbl(allom_mu, "b2Bl")[ipft]),
       lai = nplant * bleaf * SLA,
       elai = lai * clumping_factor
-    ),
+    ) %>%
+    purrr::modify(unname),
   site_lai_summary = site_lai_samples %>%
-    dplyr::group_by(site, year, ipft, hite, dbh, nplant, cohort) %>%
+    dplyr::group_by(site, year, pft, ipft, hite, dbh, nplant, cohort) %>%
     dplyr::summarize_at(
       c("lai", "elai"),
       dplyr::funs(
@@ -97,7 +101,57 @@ pre_plan <- drake_plan(
         q025 = quantile(., 0.025),
         q975 = quantile(., 0.975)
       )
-    )
+    ) %>%
+    dplyr::ungroup() %>%
+    purrr::modify(unname) %>%
+    dplyr::group_by(site, year) %>%
+    dplyr::arrange(hite) %>%
+    dplyr::mutate(
+      cum_lai = cumsum(lai_mean),
+      cum_elai = cumsum(elai_mean)
+    ) %>%
+    dplyr::ungroup(),
+  lai_observed = readr::read_csv(file_in("other_site_data/NASA_FFT_LAI_FPAR_Data.csv")) %>%
+    dplyr::group_by(site = Site_Plot) %>%
+    dplyr::summarize(
+      obs_LAI = mean(LAI_CLX_10_60_m2_m2, na.rm = TRUE),
+      obs_LAI_SD = sd(LAI_CLX_10_60_m2_m2, na.rm = TRUE),
+      obs_LAI_lo = obs_LAI - obs_LAI_SD,
+     obs_LAI_hi = obs_LAI + obs_LAI_SD
+    ) %>%
+    dplyr::filter_if(is.numeric, dplyr::all_vars(. > 0))
+)
+
+## loadd(lai_observed)
+
+site_lai_plots_template <- drake_plan(
+  siteplot = site_lai_summary %>%
+    dplyr::filter(site == "site__") %>%
+    ggplot() +
+    aes(y = hite, color = pft) +
+    geom_segment(aes(x = 0, xend = xvar__, yend = hite)) +
+    geom_point(aes(x = xvar__, y = hite)) +
+    geom_vline(aes(xintercept = obs_LAI), linetype = "solid",
+               data = lai_observed %>% dplyr::filter(site == "site__")) +
+    geom_vline(aes(xintercept = obs_LAI_lo), linetype = "dashed",
+               data = lai_observed %>% dplyr::filter(site == "site__")) +
+    geom_vline(aes(xintercept = obs_LAI_hi), linetype = "dashed",
+               data = lai_observed %>% dplyr::filter(site == "site__")) +
+    labs(x = "Leaf area index", y = "Height (m)", color = "PFT") +
+    ggtitle("site__"),
+  siteplot_png = ggsave(
+    "figures/siteplot_site___xvar__.png",
+    siteplot_site___xvar__,
+    width = 7, height = 7, units = "in"
+  )
+)
+
+site_lai_plots_plan <- evaluate_plan(
+  site_lai_plots_template,
+  rules = list(
+    site__ = selected_sites,
+    xvar__ = c("cum_lai", "cum_elai", "lai_mean", "elai_mean")
+  )
 )
 
 spec_validation_template <- drake_plan(
@@ -159,27 +213,32 @@ spec_validation_plot_plan <- evaluate_plan(
   rules = list(site__ = make.names(selected_sites))
 )
 
-current_plan <- bind_plans(pre_plan, spec_validation_plan, spec_validation_plot_plan)
+current_plan <- bind_plans(
+  pre_plan,
+  spec_validation_plan,
+  spec_validation_plot_plan,
+  site_lai_plots_plan
+)
 current_config <- drake_config(current_plan)
-make(current_plan, jobs = 8)
+make(current_plan, jobs = parallel::detectCores())
 
-ed_plan_template <- drake_plan(
-  edresult = run_ed_site_ens(
-    site = "site__",
-    trait_values = ensemble_trait_list[[ens__]],
-    outdir_prefix = file.path("testsamples", ens__),
-    end_date = "2011-12-31"
-  )
-)
+## ed_plan_template <- drake_plan(
+##   edresult = run_ed_site_ens(
+##     site = "site__",
+##     trait_values = ensemble_trait_list[[ens__]],
+##     outdir_prefix = file.path("testsamples", ens__),
+##     end_date = "2011-12-31"
+##   )
+## )
 
-ed_plan <- evaluate_plan(
-  ed_plan_template,
-  rules = list(
-    site__ = selected_sites,
-    ens__ = seq_len(nens)
-  )
-)
+## ed_plan <- evaluate_plan(
+##   ed_plan_template,
+##   rules = list(
+##     site__ = selected_sites,
+##     ens__ = seq_len(nens)
+##   )
+## )
 
-plan <- bind_plans(pre_plan, ed_plan)
-dconf <- drake_config(plan)
-make(plan, jobs = parallel::detectCores())
+## plan <- bind_plans(pre_plan, ed_plan)
+## dconf <- drake_config(plan)
+## make(plan, jobs = parallel::detectCores())
