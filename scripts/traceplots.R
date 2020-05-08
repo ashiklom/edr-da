1394c614358a92713f5e3c2b562b0ce719f257c7
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2020-05-08 12:19:36 -0400

    Traceplots for different model configs

--- scripts/traceplots.R
+++ scripts/traceplots.R
@@ -1,10 +1,12 @@
 library(magrittr)
 source("drake/functions.R")
 
-## f <- last_result_file("multi_site_pda_results")
-f <- last_result_file("multi_site_pda_results-exp")
-s <- BayesianTools::getSample(readRDS(f), coda = TRUE, thin = "auto")
-
-pdf(fs::path(fs::path_dir(f), "trace.pdf"))
-plot(s)
-dev.off()
+for (conf in c("homo-pooled", "hetero-pooled",
+               "homo-sitespecific", "hetero-sitespecific")) {
+  message("Plotting ", conf)
+  f <- last_result_file(file.path("multi_site_pda_results", conf))
+  s <- BayesianTools::getSample(readRDS(f), coda = TRUE, thin = "auto")
+  pdf(fs::path(fs::path_dir(f), "trace.pdf"))
+  plot(s)
+  dev.off()
+}
