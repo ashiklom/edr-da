4497fe11823cc7b7a7b61e6a82458780123a71f8
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2020-05-13 17:24:36 -0400

    Add parameter names to trace plots

--- scripts/traceplots.R
+++ scripts/traceplots.R
@@ -5,7 +5,9 @@ for (conf in c("homo-pooled", "hetero-pooled",
                "homo-sitespecific", "hetero-sitespecific")) {
   message("Plotting ", conf)
   f <- last_result_file(file.path("multi_site_pda_results", conf))
+  param_names <- readLines(file.path(dirname(f), "param_names.txt"))
   s <- BayesianTools::getSample(readRDS(f), coda = TRUE, thin = "auto")
+  coda::varnames(s) <- param_names
   pdf(fs::path(fs::path_dir(f), "trace.pdf"))
   plot(s)
   dev.off()
